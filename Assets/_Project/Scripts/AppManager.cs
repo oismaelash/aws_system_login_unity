eff262ab48f845fa1eab273745e7b4e1e8f6d8b5
Ismael Nascimento <contato@ismaelnascimento.com>
2019-08-24 20:14:45 -0300

    initial commit

---
+++ Assets/_Project/Scripts/AppManager.cs
@@ -0,0 +1,56 @@
+﻿using Proyecto26;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour
+{
+    [Header("App config")]
+    [SerializeField] private string apiUrl;
+    [SerializeField] private string tableDynamoDB;
+    [Header("Inputs user")]
+    [SerializeField] private string usernameUser;
+    [SerializeField] private string passwordUser;
+
+    [ContextMenu("Login")]
+    private void Login()
+    {
+        LoginSignupModel loginSignupModel = new LoginSignupModel
+        {
+            table = tableDynamoDB,
+            username = usernameUser,
+            password = passwordUser
+        };
+
+        string jsonBody = JsonUtility.ToJson(loginSignupModel);
+
+        RestClient.Post(apiUrl, jsonBody).Then(response =>
+        {
+            print($"response:: {response.Text}");
+
+        }).Catch(error =>
+        {
+            print($"error:: {error.Message}");
+        });
+    }
+
+    [ContextMenu("Signup")]
+    private void Signup()
+    {
+        LoginSignupModel loginSignupModel = new LoginSignupModel
+        {
+            table = tableDynamoDB,
+            username = usernameUser,
+            password = passwordUser
+        };
+
+        string jsonBody = JsonUtility.ToJson(loginSignupModel);
+
+        RestClient.Put(apiUrl, jsonBody).Then(response =>
+        {
+            print($"response:: {response.Text}");
+
+        }).Catch(error =>
+        {
+            print($"error:: {error.Message}");
+        });
+    }
+}
---
+++ Assets/_Project/Scripts/LoginSignupModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+[Serializable]
+public class LoginSignupModel
+{
+    public string table;
+    public string username;
+    public string password;
+}
